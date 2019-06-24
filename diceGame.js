d630d661f915fcf53f4fa0498bb86133afcf811d
cfirgens <firgenscj@gmail.com>
2019-06-24 16:51:49 -0500

    Started front-end; landing page done; removed enemy crit dmg; tweaked combat functionality, user now rolls every attack;

--- diceGame.js
+++ diceGame.js
@@ -2,31 +2,28 @@
 runGame()
 
 function runGame(){
-    let playerHP = 0;
-    let playerAttack = 0;
-    let playerDefense = 0;
-    let playerEvade = 0;
-    let wizardHP = 30;
-    let griffonHP = 20;
     let userClass = "";
-    // let peasent, wizard, knight;
-    let playerCritDmg = playerAttack*1.5;
     let selectedAbility;
 
-    console.log("Lore.... click below to roll hp")
+    document.write("Lore.... click below to roll hp")
     
     userClass = chooseClass(); 
-    getStats(userClass);
 
-    console.log(playerHP);
+    let playerHP = getPlayerHP(userClass);
+    // let playerAttack = getPlayerAttack(userClass);
+    let playerDefense = getPlayerDefense(userClass);
+    let playerEvade = getPlayerEvade(userClass);
 
-    console.log("You fight the mighty griffon");
 
-    fightGriffon();
+    document.write(playerHP);
 
-    console.log(playerHP);
+    document.write("You fight the mighty griffon");
 
-    console.log("Lore... save the prince and defeat the red wizard!")
+    fightGriffon(userClass);
+
+    document.write(playerHP);
+
+    document.write("Lore... save the prince and defeat the red wizard!")
 
     fightWizard();
 
@@ -51,49 +48,88 @@ function runGame(){
                 selectedClass = "peasant"; 
                      break;
         }
-        console.log(classLore);
+        document.write(classLore);
         return selectedClass;
     }
 
-    function getStats(userClass){\
+    function getPlayerHP(userClass){
         // declare variables
         if(userClass === "knight"){
             playerHP = rollDie(12)+20;
+        }
+
+        else if (userClass === "wizard"){
+            playerHP = rollDie(12)+10;
+        }
+
+        else{
+            playerHP = rollDie(12)+15;
+        }
+        return playerHP;
+    }
+
+    function getPlayerAttack(userClass){
+        // declare variables
+        if(userClass === "knight"){
             playerAttack = rollDie(6)+2;
+        }
+
+        else if (userClass === "wizard"){
+            playerAttack = rollDie(12)+4;
+        }
+
+        else{
+            playerAttack = rollDie(8)+3;
+        }
+        return playerAttack;
+    }
+
+    function getPlayerDefense(userClass){
+        // declare variables
+        if(userClass === "knight"){
             playerDefense = 8;
+        }
+        else if (userClass === "wizard"){
+            playerDefense = 4;
+        }
+        else{
+            playerDefense = 6;
+        }
+        return playerDefense;
+    }
+
+    function getPlayerEvade(userClass){
+        // declare variables
+        if(userClass === "knight"){
             playerEvade = 30;
         }
         else if (userClass === "wizard"){
-            playerHP = rollDie(12)+10;
-            playerAttack = rollDie(12)+4;
-            playerDefense = 4;
             playerEvade = 40;
         }
         else{
-            playerHP = rollDie(12)+15;
-            playerAttack = rollDie(8)+3;
-            playerDefense = 6;
             playerEvade = 50;
         }
+        return playerEvade;
     }
+
 
     function userFightOptions(){
         let selectedAbility = "";
         let userFightOptionsPrompt = prompt("What action would you like to take? (ie: Attack, Defend, Evade)");
         switch(userFightOptionsPrompt){
             case "Attack":
-                // selectedAbility = playerAttack + console.log('You attack and deal:'+ playerAttack);
+                // selectedAbility = playerAttack + document.write('You attack and deal:'+ playerAttack);
                 selectedAbility= "Attack";
                     break;
         
             case "Defend":
-                //selectedAbility = playerDefense + console.log('You defend against the attack and take' + playerDefense +' less damage');
+                //selectedAbility = playerDefense + document.write('You defend against the attack and take' + playerDefense +' less damage');
                 selectedAbility = "Defend";
                     break;
 
             case "Evade":
                 selectedAbility = "Evade";
-                // selectedAbility = playerEvade + console.log('You attempt to evade against the next attack against you');
+                // selectedAbility = playerEvade + document.write('You attempt to evade against the next attack against you');
                     break;
             default:
                 selectedAbility = "Fear";
@@ -107,85 +143,130 @@ function runGame(){
         return total;
     }
 
-    function fightGriffon(){
-        let griffonAttack = rollDie(8);
+    function fightGriffon(userClass){
+        let critMultiplier = 1.5;
+        let playerCrit = 0;
+        let griffonHP = 20;
             while (griffonHP > 0){
+                let griffonAttack = rollDie(8);
                     if (playerHP < 0){
-                        console.log("The griffon has killed you. A hunter discovers your corpse or what's left of it. The wizard has won.")
+                        document.write("The griffon has killed you. A hunter discovers your corpse or what's left of it.")
+                        location.reload();
+
                             break;
-                    }
-                    if (griffonAttack >=8){
-                        console.log("The griffon has critically hit you.");
-                        griffonAttack = griffonCrit;
                     }
 
                    let selectedAbility = userFightOptions();
 
-                    if (selectedAbility = Attack){
+                    if (selectedAbility === "Attack"){
+                        let playerAttack = getPlayerAttack(userClass);
+                        let playerCritDmg = playerAttack*critMultiplier;
+                        playerCrit = rollDie(100);
                         if (playerCrit>80){
-                            playerCrit = rollDie(100);
-                            console.log("You have critically hit!")
+                            document.write("You have critically hit!")
+                            document.write("The griffon took " + playerCritDmg + " damage.");
+                            document.write("The griffon flinches and deals half damage.")
+                            document.write("The griffon dealt: " + griffonAttack*.5 + " damage.");
                             griffonHP= griffonHP - playerCritDmg;
-                            playerHP = playerHP - griffonAttack;
-
-                        }
+                            playerHP = playerHP - griffonAttack*(.5);
+                                }
+                    
                         else{
+                            document.write("You hit the griffon for: " + playerAttack);
+                            document.write("The griffon hits you for: " + griffonAttack);
                             griffonHP = (griffonHP - playerAttack);
                             playerHP = (playerHP - griffonAttack);
                         }
                     }
-                    else if (selectedAbility = Defend){
+                    else if (selectedAbility === "Defend"){
                         playerHP = playerHP - (griffonAttack - playerDefense);
+                        document.write("You defend for: " + playerDefense + " damage.")
+                        document.write("The griffon hits you for: " + (griffonAttack - playerDefense)); 
                     }
-                    else if (selectedAbility = Evade){
+                    else if (selectedAbility === "Evade"){
                         playerEvade= rollDie(70)+playerEvade;
                         if (playerEvade>80){
                             
-                            console.log("You have evaded the griffons attack!");
+                            document.write("You have evaded the griffons attack and regained 5 hp");
+                            playerHP += 5;
                         }
                         else{
-                            console.log("You tripped over a rock, and recieved a critical blow");
-                            playerHP = playerHP - (griffonAttack*1.5);
+                            document.write("You tripped over a rock, and recieved a critical blow");
+                            document.write("The griffon critically hits you for: " + (griffonAttack*critMultiplier));
+                            playerHP = playerHP - (griffonAttack*critMultiplier);
                         }
                     }
-                    else if(selectedAbility ="Fear"){
-                        console.log("You froze in fear and took double damage.");
+                    else if(selectedAbility === "Fear"){
+                        document.write("You froze in fear and took double damage.");
+                        document.write("Griffon dealt: "+ griffonAttack*2 + " damage.");
                         playerHP= playerHP - (griffonAttack*2);
                         }
 
+                    document.write("The griffons hp is: " + griffonHP);
+                    document.write("Your hp is: " + playerHP);
                     }
 
-        
-
-                console.log('The griffons hp is:' + griffonHP);
-                console.log('Your hp is:' + playerHP);
+                document.write("You defeated the mighty griffon and take his head back as a proof of this venerable exploit")
+                
+                return playerHP;
             }
-        console.log("You defeated the mighty griffon and take his head back as a proof of this venerable exploit")
-        return playerHP;
-    }
+
 
     function fightWizard(){
+        let critMultiplier = 1.5;
+        let playerCrit = 0;
+        let wizardHP = 30;
+        let playerCritDmg = playerAttack*1.5;
             while (wizardHP>0){
                 let wizardAttack = rollDie(10);
 
                     if (playerHP < 0){
-                        console.log("The wizard has killed you and you have failed yourself, the princess and the kingdom")
+                        document.write("The wizard has killed you and you have failed yourself, the princess and the kingdom")
                             break;
                     }
-                    if (wizardAttack >=10){
-                        console.log("The wizard has critically hit you.");
-                        wizardAttack = 15;
-                    
+
+                    let selectedAbility = userFightOptions();
+
+                    if (selectedAbility === "Attack"){
+                        playerCrit = rollDie(100);
+                        if (playerCrit>80){
+                            document.write("You have critically hit!")
+                            wizardHP= wizardHP - playerCritDmg;
+                            playerHP = playerHP - wizardAttack;
+
+                        }
+                        else{
+                            wizardHP = (wizardHP - playerAttack);
+                            playerHP = (playerHP - wizardAttack);
+                        }
                     }
+                    else if (selectedAbility === "Defend"){
+                        playerHP = playerHP - (wizardAttack - playerDefense);
+                    }
+                    else if (selectedAbility === "Evade"){
+                        playerEvade= rollDie(70)+playerEvade;
+                        if (playerEvade>80){
+                            
+                            document.write("You have evaded the wizards fireball attack!");
+                        }
+                        else{
+                            document.write("You tripped over a rock, and recieved a critical blow");
+                            playerHP = playerHP - (wizardAttack*1.5);
+                        }
+                    }
+                    else if(selectedAbility === "Fear"){
+                        document.write("You froze in fear and took double damage.");
+                        playerHP= playerHP - (wizardAttack*2);
+                        }
 
                 playerHP =(playerHP - wizardAttack);
                 wizardHP=(wizardHP - playerAttack);
                 
-                console.log('The wizards hp is:' + wizardHP);
-                console.log('Your hp is:' + playerHP);
+                document.write('The wizards hp is:' + wizardHP);
+                document.write('Your hp is:' + playerHP);
                     
             }
-        console.log('You have defeated the mighty wizard. Finding the princess in the dungeon you unshackle her and bring her back to her rightful place in the castle, you have completed your quest!');
+        document.write('You have defeated the mighty wizard. Finding the princess in the dungeon you unshackle her and bring her back to her rightful place in the castle, you have completed your quest!');
         return playerHP;
 
     }
